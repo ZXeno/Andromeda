3456882cfc60860f545b4d78c3f2a1e2cc59170e
Jonathan Cain - portable <xivdot@gmail.com>
2014-09-03 13:55:15 -0700

    added more substance to NetworkConnection class, but still unfinished due to time constraints.

--- Andromeda/Andromeda/NetworkConnection.cs
+++ Andromeda/Andromeda/NetworkConnection.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net;
+using System.Net.Sockets;
 using System.Net.NetworkInformation;
 using System.Collections.Generic;
 using System.Text;
@@ -8,29 +9,62 @@ namespace Andromeda
 {
     public class NetworkConnection
     {
-        Ping pingSender;
-        PingOptions pingOptions;
+        private Ping ping;
+        private PingOptions pingOptions;
 
         public NetworkConnection()
         {
-            pingSender = new Ping();
+            ping = new Ping();
             pingOptions = new PingOptions();
             pingOptions.DontFragment = true;
         }
 
         public void PingTest(string hostname)
         {
+            string returnMsg = "";
+            PingReply pr = ping.Send(hostname);
+            try { }
+            catch (PingException e)
+            {
 
+            }
+            catch (SocketException e)
+            {
+                
+            }
+
+            
         }
 
         public void PingTest(List<string> hostnames)
         {
+            try { }
+            catch (PingException e)
+            {
 
+            }
+            catch (SocketException e)
+            {
+
+            }
         }
 
         public void PingTest(string[] hostnamearray)
         {
+            try { }
+            catch (PingException e)
+            {
 
+            }
+            catch (SocketException e)
+            {
+
+            }
+        }
+
+        public bool HasConnection()
+        {
+            return false;
         }
     }
 }
