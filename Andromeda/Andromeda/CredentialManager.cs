76ff3c29672b9a37298a2a655ea25e5ff5d82788
Jonathan Cain <xivdot@gmail.com>
2014-09-09 22:30:20 -0700

    Placeholder for future credential management

---
+++ Andromeda/Andromeda/CredentialManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda
+{
+    public static class CredentialManager
+    {
+
+        public static string GetUser()
+        {
+            return "null";
+        }
+
+        public static string GetPass()
+        {
+            return "null";
+        }
+    }
+}
