d2a5b00748e599a465c42dea7dd2f700c7f3f08d
Jonathan Cain <xivdot@gmail.com>
2014-09-10 00:20:57 -0700

    Check for AD credentials, check if local account

--- Andromeda/Andromeda/CredentialManager.cs
+++ Andromeda/Andromeda/CredentialManager.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
 
 namespace Andromeda
 {
     public static class CredentialManager
     {
+
+        public static string UserName { get; set; }
+
+        public static string GetDomain()
+        {
+            return Environment.UserDomainName;
+        }
 
         public static string GetUser()
         {
@@ -17,6 +24,41 @@ namespace Andromeda
         public static string GetPass()
         {
             return "null";
+        }
+
+        public static bool DoesUserExistInActiveDirectory(string userName)
+        {
+            try
+            {
+                using (var domainContext = new PrincipalContext(ContextType.Domain, Environment.UserDomainName))
+                {
+                    using (var foundUser = UserPrincipal.FindByIdentity(domainContext, IdentityType.SamAccountName, userName))
+                    {
+                        return foundUser != null;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+
+
+        }
+
+        public static bool IsUserLocal(string userName)
+        {
+            bool exists = false;
+
+            using (var domainContext = new PrincipalContext(ContextType.Machine))
+            {
+                using (var foundUser = UserPrincipal.FindByIdentity(domainContext, IdentityType.SamAccountName, userName))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
