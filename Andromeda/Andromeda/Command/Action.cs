ecd110e7567a2c5b96273e3f3f12a23ebd087286
Jonathan Cain - portable <xivdot@gmail.com>
2014-09-13 17:04:56 -0700

    Added "Debug" to ActionGroup enum

--- Andromeda/Andromeda/Command/Action.cs
+++ Andromeda/Andromeda/Command/Action.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-public enum ActionGroup { Schedule, Agent, Health, Advertisement, Other }
+public enum ActionGroup { Schedule, Agent, Health, Advertisement, Other, Debug }
 
 namespace Andromeda.Command
 {
