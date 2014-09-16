5172195b97c58ca91df55b72b381393d7aed588b
Jonathan Cain - portable <xivdot@gmail.com>
2014-09-16 11:58:18 -0700

    started a class to fix the CE DeviceID.XML corrupt file error

---
+++ Andromeda/Andromeda/Command/FixCEDeviceID.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Andromeda.Command
+{
+    class FixCEDeviceID : Action
+    {
+        private NetworkConnections netconn;
+
+        public FixCEDeviceID()
+        {
+            ActionName = "Fix CE DeviceID XML File";
+            Desctiption = "Repairs the DeviceID.XML file for a given device.";
+            Category = ActionGroup.Other;
+            netconn = new NetworkConnections();
+        }
+
+        public override string RunCommand(string a)
+        {
+            string returnMsg = "";
+
+
+            return returnMsg;
+        }
+    }
+}
