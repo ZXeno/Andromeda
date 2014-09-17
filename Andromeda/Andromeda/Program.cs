32e48e5b4a4a5965c5c3e142868c9041c7000234
Jonathan Cain <xivdot@gmail.com>
2014-09-16 23:19:33 -0700

    constructor method

--- Andromeda/Andromeda/Program.cs
+++ Andromeda/Andromeda/Program.cs
@@ -8,7 +8,12 @@ namespace Andromeda
 {
     public class Program
     {
+        //CredentialManager credman;
 
+        public Program()
+        {
+            //credman = new CredentialManager();
+        }
 
     }
 }
