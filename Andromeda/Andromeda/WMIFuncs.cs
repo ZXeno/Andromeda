64517a35d832b3a0a1397c6ad4b2a61350b96044
Jonathan Cain <xivdot@gmail.com>
2014-09-17 19:14:46 -0700

    initial commit - moved WMI functions to this class

---
+++ Andromeda/Andromeda/WMIFuncs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Management;
+
+namespace Andromeda
+{
+    public class WMIFuncs
+    {
+
+
+        public WMIFuncs()
+        {
+
+        }
+
+        public ManagementScope ConnectToRemoteWMI(string hostname, string scope, ConnectionOptions options)
+        {
+            ManagementScope wmiscope = new ManagementScope("\\\\" + hostname + scope, options);
+            wmiscope.Connect();
+            return wmiscope;
+        }
+
+        public bool CheckWMIAccessible(string hostname, string scope, ConnectionOptions options)
+        {
+            ManagementScope wmiscope = ConnectToRemoteWMI(hostname, scope, options);
+            if (wmiscope.IsConnected) { ResultConsole.AddConsoleLine("Connected to WMI scope " + wmiscope.Path); }
+            else { ResultConsole.AddConsoleLine("Connection to WMI scope " + wmiscope.Path + " failed."); }
+            return wmiscope.IsConnected;
+        }
+
+        public ManagementScope ConnectToSCCMscope(string hostname)
+        {
+            try
+            {
+                ManagementScope ccmscope = new ManagementScope("\\\\" + hostname + "root\\ccm");
+                ccmscope.Connect();
+                return ccmscope;
+            }
+            catch (ManagementException e)
+            {
+                ResultConsole.AddConsoleLine("Failed to connect.");
+                ResultConsole.AddConsoleLine(e.Message);
+                throw;
+            }
+        }
+    }
+}
