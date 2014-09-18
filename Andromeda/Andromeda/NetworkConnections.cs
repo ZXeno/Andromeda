c89f49379e2b62e68c06f5b840b87f2a101e9323
Jonathan Cain <xivdot@gmail.com>
2014-09-17 19:16:48 -0700

    removed WMI functionality

--- Andromeda/Andromeda/NetworkConnections.cs
+++ Andromeda/Andromeda/NetworkConnections.cs
@@ -30,36 +30,7 @@ namespace Andromeda
             return pr;
         }
 
-        public ManagementScope ConnectToRemoteWMI(string hostname, string scope, ConnectionOptions options)
-        {
-            ManagementScope wmiscope = new ManagementScope("\\\\" + hostname + scope, options);
-            wmiscope.Connect();
-            return wmiscope;
-        }
-
-        public bool CheckWMIAccessible(string hostname, string scope, ConnectionOptions options)
-        {
-            ManagementScope wmiscope = ConnectToRemoteWMI(hostname, scope, options);
-            if (wmiscope.IsConnected) { ResultConsole.AddConsoleLine("Connected to WMI scope " + wmiscope.Path); }
-            else { ResultConsole.AddConsoleLine("Connection to WMI scope " + wmiscope.Path + " failed."); }
-            return wmiscope.IsConnected;
-        }
-
-        public ManagementScope ConnectToSCCMscope(string hostname)
-        {
-            try
-            {
-                ManagementScope ccmscope = new ManagementScope("\\\\" + hostname + "root\\ccm");
-                ccmscope.Connect();
-                return ccmscope;
-            }
-            catch (ManagementException e)
-            {
-                ResultConsole.AddConsoleLine("Failed to connect.");
-                ResultConsole.AddConsoleLine(e.Message);
-                throw;
-            }
-        }
+        
 
 
     }
