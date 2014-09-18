59ed3a0ff9793e26497f9c303eba4a5d3195e36f
Jonathan Cain <xivdot@gmail.com>
2014-09-17 19:15:01 -0700

    updated to reflect WMI changes

--- Andromeda/Andromeda/Command/DebugToConsole.cs
+++ Andromeda/Andromeda/Command/DebugToConsole.cs
@@ -10,6 +10,7 @@ namespace Andromeda.Command
     class DebugToConsole : Action
     {
         NetworkConnections netconn;
+        WMIFuncs wmi;
         ConnectionOptions connOps;
 
         public DebugToConsole()
@@ -20,15 +21,16 @@ namespace Andromeda.Command
             netconn = new NetworkConnections();
             connOps = new ConnectionOptions();
             connOps.Impersonation = ImpersonationLevel.Impersonate;
+            wmi = new WMIFuncs();
         }
 
         public override void RunCommand(string input)
         {
             foreach (string d in ParseDeviceList(input))
             {
-                if (netconn.CheckWMIAccessible(d, "\\root\\CIMV2", connOps))
+                if (wmi.CheckWMIAccessible(d, "\\root\\CIMV2", connOps))
                 {
-                    ManagementScope testscope = netconn.ConnectToRemoteWMI(d, "\\root\\CIMV2", connOps);
+                    ManagementScope testscope = wmi.ConnectToRemoteWMI(d, "\\root\\CIMV2", connOps);
                     if (testscope != null)
                     {
                         ResultConsole.AddConsoleLine("Successfull connection");
