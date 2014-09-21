01ec33c3b32ee99e367ec035c28a084d3b4dc9ed
Jonathan Cain - portable <xivdot@gmail.com>
2014-09-20 18:03:24 -0700

    Now provides impersonated credentials as a call as well.

--- Andromeda/Andromeda/Credentials/CredToken.cs
+++ Andromeda/Andromeda/Credentials/CredToken.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Security;
 using System.Runtime.InteropServices;
+using System.Management;
 
 namespace Andromeda.Credentials
 {
@@ -11,10 +12,12 @@ namespace Andromeda.Credentials
         private string _usr;
         private SecureString _pw;
         private string _domain;
+        private bool _canImpersonate;
 
         public string User { get { return _usr; } }
         public SecureString SecurePassword { get { return _pw; } }
         public string Domain { get { return _domain; } }
+        public bool CanImpersonate { get { return _canImpersonate; } }
 
         public CredToken() { }
 
@@ -23,6 +26,15 @@ namespace Andromeda.Credentials
             _usr = user;
             _domain = domain;
             _pw = Password;
+            _canImpersonate = false;
+        }
+
+        public CredToken(string domain, string user, SecureString Password, bool canimpersonate)
+        {
+            _usr = user;
+            _domain = domain;
+            _pw = Password;
+            _canImpersonate = canimpersonate;
         }
 
         public string GetInsecurePasswordString()
@@ -37,6 +49,28 @@ namespace Andromeda.Credentials
             {
                 Marshal.ZeroFreeGlobalAllocUnicode(unmanagedString);
             }
+        }
+
+        public ConnectionOptions GetNonImpersonatedConnOptions()
+        {
+            ConnectionOptions connOps = new ConnectionOptions();
+
+            connOps.SecurePassword = this.SecurePassword;
+            connOps.Username = this.User;
+
+            return connOps;
+        }
+
+        public ConnectionOptions GetImpersonatedConnOptions()
+        {
+            ConnectionOptions connOps = new ConnectionOptions();
+            connOps.Impersonation = ImpersonationLevel.Impersonate;
+            connOps.Authentication = AuthenticationLevel.PacketPrivacy;
+            connOps.EnablePrivileges = true;
+            connOps.Username = this.User;
+            connOps.SecurePassword = this.SecurePassword;
+
+            return connOps;
         }
     }
 }
