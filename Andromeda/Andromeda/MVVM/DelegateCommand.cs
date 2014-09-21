aae478af39d8c6afc73d70edc1f56c215e67e7e7
Jonathan Cain - portable <xivdot@gmail.com>
2014-09-20 18:02:02 -0700

    implemented first pass of MVVM framework

---
+++ Andromeda/Andromeda/MVVM/DelegateCommand.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using System.Text;
+
+namespace Andromeda.MVVM
+{
+    public class DelegateCommand : ICommand
+    {
+        private readonly Action _action;
+
+        public DelegateCommand(Action action)
+        {
+            _action = action;
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+#pragma warning disable 67
+        public event EventHandler CanExecuteChanged;
+#pragma warning restore 67
+    }
+
+}
---
+++ Andromeda/Andromeda/MVVM/ObservableObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+using System.ComponentModel;
+
+namespace Andromeda.MVVM
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChangedEvent(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
