bfbb9bd1debab3cdd2c8c6bac492aa7b88cacddd
Jonathan Cain <Jonathan Cain>
2016-06-07 22:37:32 -0700

    missed in commit 2fb2518

--- Andromeda/Andromeda-Actions-Core/Model/Configuration.cs
+++ Andromeda/Andromeda-Actions-Core/Model/Configuration.cs
@@ -5,12 +5,9 @@
         public string DataFilePath { get; set; }
         public bool SaveOfflineComputers { get; set; }
         public bool SaveOnlineComputers { get; set; }
-        public bool AlwaysDumpConsoleHistory { get; set; }
         public string ResultsDirectory { get; set; }
         public string ComponentDirectory { get; set; }
         public string FailedConnectListFile { get; set; }
         public string SuccessfulConnectionListFile { get; set; }
-        public bool FirstTimeAutoFixWmiCheck { get; set; }
-        public bool AutomaticallyFixWmi { get; set; }
     }
 }
