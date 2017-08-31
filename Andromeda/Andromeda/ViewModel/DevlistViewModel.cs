ff98faba175eda414a7c0177dfbfb4b4275bb250
Jonathan Cain <xivdot@gmail.com>
2017-08-30 23:23:29 -0700

    Added ability to view device list of running action

---
+++ Andromeda/Andromeda/ViewModel/DevlistViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+using AndromedaCore.Infrastructure;
+using AndromedaCore.ViewModel;
+
+namespace Andromeda.ViewModel
+{
+    public class DevlistViewModel : RequestCloseViewModel
+    {
+        private string _rawDevlistString;
+        public string DevlistString
+        {
+            get => _rawDevlistString;
+            set
+            {
+                _rawDevlistString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ICommand _close;
+        public ICommand CloseCommand
+        {
+            get
+            {
+                if (_close == null)
+                {
+                    _close = new DelegateCommand(param => Close(), param => true);
+                }
+                return _close;
+            }
+        }
+
+        public DevlistViewModel(string deviceListString)
+        {
+            DevlistString = deviceListString;
+        }
+
+        public void Close()
+        {
+            OnRequestClose(EventArgs.Empty);
+        }
+    }
+}
--- Andromeda/AndromedaCore/Model/RunningActionTask.cs
+++ Andromeda/AndromedaCore/Model/RunningActionTask.cs
@@ -1,4 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AndromedaCore.Model
 {
@@ -6,8 +9,36 @@ namespace AndromedaCore.Model
     {
         public Task ThisActionsTask { get; set; }
         public IAction RunningAction { get; set; }
-        public string RawDeviceListString { get; set; }
         public string RunningActionName { get; set; }
         public int ThreadId { get; set; }
+        public List<String> DeviceList { get; private set; }
+        private string _rawDeviceString;
+        public string RawDeviceListString
+        {
+            get => _rawDeviceString;
+            set
+            {
+                _rawDeviceString = value;
+                ParseDeviceString(_rawDeviceString);
+            }
+        }
+
+        private void ParseDeviceString(string deviceString)
+        {
+            var devList = new List<string>(deviceString.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries));
+
+            var resultList = new List<string>();
+
+            foreach (var d in devList)
+            {
+                var t = d;
+
+                t = new string(t.ToCharArray().Where(c => !char.IsWhiteSpace(c)).ToArray());
+
+                resultList.Add(t);
+            }
+
+            DeviceList = resultList;
+        }
     }
 }
